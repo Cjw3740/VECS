02f043851c5301315948211988d6d8a35f0c7cec
Cjw3740 <35120764+Cjw3740@users.noreply.github.com>
2018-10-25 19:12:52 -0400

    manual control working
    got the shift out working so manual control works. Odd issue with the rtc being set wrong which caused issues with the sensors updating. Manually set the time and it fixed it. Will have to keep an eye on it.

--- arduino_V4.ino
+++ arduino_V4.ino
@@ -12,7 +12,10 @@
 
 
 RTC_DS3231 rtc;
+dht DHT;
 
+#define DHT22_PIN_1 5
+#define DHT22_PIN_2 6
 
 const byte numChars = 32;
 char receivedChars[numChars];
@@ -20,6 +23,8 @@ char receivedChars[numChars];
 boolean newData = false;
 
 int num_relays = 16;
+
+int last_sensor_read = 0;
 
 float t1 = 75.5;
 float h1 = 65.5;
@@ -42,7 +47,15 @@ String CurrentORState = "NN";
 
 
 String RelayState = "0000000000000000";
+int RS_first = 0b00000000;
+int RS_last = 0b00000000;
 String last_RelayState = RelayState;
+int SO_latchPin = 8;
+int SO_clockPin = 12;
+int SO_dataPin = 11;
+
+
+
 
 String NN = "2222222222222222";
 String HN = "2222222222222222";
@@ -54,35 +67,51 @@ String ND = "2222222222222222";
 String HD = "2222222222222222";
 String CD = "2222222222222222";
 
-String ORStates[9] = {NN,HN,CN,NW,HW,CW,ND,HD,CD};
+String ORStates[9] = {NN, HN, CN, NW, HW, CW, ND, HD, CD};
 int ORState_num = 0;
 
-String ToDo[] = {"0823451000000000000000","2030110000000000000000"};
+String ToDo[] = {"0823451000000000000000", "2030110000000000000000"};
 
 
 
 
 
-//***** Setup *****
+
+
+
+
+//************ Setup ************
 void setup() {
-    Serial.begin(9600);
-    
-    //Serial.println("<Arduino is ready>");
+  Serial.begin(9600);
 
 
 
-// start the rtc, and if it can't let the pi know
- if (! rtc.begin()) {
+  // start the rtc, and if it can't let the pi know
+  if (! rtc.begin()) {
     Serial.println("Couldn't find RTC");
-    while (1);
+    //while (1);
   }
-//sets system time from RTC on bootup
-DateTime now = rtc.now();
-setTime(now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year());
-    
-     
-}
+  //sets system time from RTC on bootup
+  DateTime now = rtc.now();
+  setTime(now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year());
 
+
+  pinMode(SO_latchPin, OUTPUT);
+  pinMode(SO_clockPin, OUTPUT);
+  pinMode(SO_dataPin, OUTPUT);
+
+
+  //shifts out the initial relay state on bootup
+  digitalWrite(SO_clockPin, LOW);
+  digitalWrite(SO_latchPin, LOW);
+  shiftOut(SO_dataPin, SO_clockPin, LSBFIRST, 0b01111111); //relays 9-16
+  shiftOut(SO_dataPin, SO_clockPin, LSBFIRST, 0b10000000); //relays 1-8
+  digitalWrite(SO_latchPin, HIGH);
+
+
+
+}
+//*******************************
 
 
 
@@ -91,12 +120,12 @@ setTime(now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year
 
 void loop() {
   last_RelayState = RelayState;
-    //getSensorData();
-    getInput();
-    instruction_handle();
-    determineRS_shouldbe();   //use the MC, ToDo list, and override state to determine what the relay state should be
-    update_RS();     //if RS changed, shift out
-    
+  getSensorData();
+  getInput();
+  instruction_handle();
+  determineRS_shouldbe();   //use the MC, ToDo list, and override state to determine what the relay state should be
+  update_RS();     //if RS changed, shift out
+
 }
 //********************************
 
@@ -104,34 +133,34 @@ void loop() {
 
 //Recieves instructions from the pi as defined by start/end brakets <>
 void getInput() {
-    static boolean recvInProgress = false;
-    static byte ndx = 0;
-    char startMarker = '<';
-    char endMarker = '>';
-    char rc;
- 
-    while (Serial.available() > 0 && newData == false) {
-        rc = Serial.read();
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
 
-        if (recvInProgress == true) {
-            if (rc != endMarker) {
-                receivedChars[ndx] = rc;
-                ndx++;
-                if (ndx >= numChars) {
-                    ndx = numChars - 1;
-                }
-            }
-            else {
-                receivedChars[ndx] = '\0'; // terminate the string
-                recvInProgress = false;
-                ndx = 0;
-                newData = true;
-            }
+  while (Serial.available() > 0 && newData == false) {
+    rc = Serial.read();
+
+    if (recvInProgress == true) {
+      if (rc != endMarker) {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
         }
-        else if (rc == startMarker) {
-            recvInProgress = true;
-        }
+      }
+      else {
+        receivedChars[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
     }
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
 }
 
 
@@ -141,118 +170,154 @@ void getInput() {
 
 // Handles instructions from the Pi
 void instruction_handle() {
-    if (newData == true) {
+  if (newData == true) {
 
-      if(String(receivedChars)[0] == 'G'){
-        //do get things
-        if(String(receivedChars)[1] == 'A'){
-          YYYY = year();
-          MM = month();
-          DD = day();
-          hh = hour();
-          mm = minute();
-          ss = second();
-          // format: YYYY:MM:DD:HH:mm:ss-RelayState-t1/h1:t2/h2-MC-OR
-          String reply = String(YYYY)+":"+String(MM)+":"+String(DD)+":"+String(hh)+":"+String(mm)+":"+String(ss)+"-"+RelayState+"-"+String(t1)+"/"+String(h1)+":"+String(t2)+"/"+String(h2)+"-"+String(MC)+"-"+CurrentORState;
-          Serial.println(reply);
-    
-        } else if(String(receivedChars)[1] == 'T'){
-          //get the time from the rtc and set the system time to it
-          DateTime now = rtc.now();
-          setTime(now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year());
-        }
+    if (String(receivedChars)[0] == 'G') {
+      //do get things
+      if (String(receivedChars)[1] == 'A') {
+        YYYY = year();
+        MM = month();
+        DD = day();
+        hh = hour();
+        mm = minute();
+        ss = second();
+        // format: YYYY:MM:DD:HH:mm:ss-RelayState-t1/h1:t2/h2-MC-OR
+        String reply = String(YYYY) + ":" + String(MM) + ":" + String(DD) + ":" + String(hh) + ":" + String(mm) + ":" + String(ss) + "-" + RelayState + "-" + String(t1) + "/" + String(h1) + ":" + String(t2) + "/" + String(h2) + "-" + String(MC) + "-" + CurrentORState;
+        Serial.println(reply);
 
-      
-      } else if(String(receivedChars)[0] == 'S'){
-        //do set things
-        if(String(receivedChars)[1]=='T'){
-          //set the date and time  format: STYYYYMMDDhhmmss
-          YYYY = String(receivedChars).substring(2,6).toInt();
-          MM = String(receivedChars).substring(6,8).toInt();
-          DD = String(receivedChars).substring(8,10).toInt();
-          hh = String(receivedChars).substring(10,12).toInt();
-          mm = String(receivedChars).substring(12,14).toInt();
-          ss = String(receivedChars).substring(14,16).toInt();
-          
-          setTime(hh, mm, ss, DD, MM, YYYY);
-          rtc.adjust(DateTime(YYYY, MM, DD, hh, mm, ss));   
-          
-        } else if(String(receivedChars)[1] == 'O'){
-          //sets the override thresholds in format:SOTH.HTL.LHH.HHL.L
-          TH = String(receivedChars).substring(2,6).toFloat();
-          TL = String(receivedChars).substring(6,10).toFloat();
-          HH = String(receivedChars).substring(10,14).toFloat();
-          HL = String(receivedChars).substring(14,18).toFloat();
-          
-        }else if(String(receivedChars)[1] == 'R'){
-          //set the override relay states
-          //send SRSTART
-          //then the relay states in order
-          //then SRSTOP
-          
-        }else if(String(receivedChars)[1] == 'P'){
-          //set main Program or ToDo list
-          //send SPSTART
-          //Then the relay state should be list, in chronological order
-          //Then SPSTOP
-      }
-      
-
-      
-      }else if(String(receivedChars).charAt(0) == 'M'){
-        //do manual control things
-        if(String(receivedChars).charAt(1)=='0'){
-          MC = 0;
-          Serial.println("MCOFF");
-        } else if(String(receivedChars).charAt(1) == '1'){
-          MC = 1;
-          Serial.println("MCON");
-        }else if(String(receivedChars).charAt(1) == 'R'){
-          RelayState = String(receivedChars).substring(2);
-          Serial.println(receivedChars);
+      } else if (String(receivedChars)[1] == 'T') {
+        //get the time from the rtc and set the system time to it
+        DateTime now = rtc.now();
+        setTime(now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year());
       }
 
-          
-        }
-    
-    
+
+    } else if (String(receivedChars)[0] == 'S') {
+      //do set things
+      if (String(receivedChars)[1] == 'T') {
+        //set the date and time  format: STYYYYMMDDhhmmss
+        YYYY = String(receivedChars).substring(2, 6).toInt();
+        MM = String(receivedChars).substring(6, 8).toInt();
+        DD = String(receivedChars).substring(8, 10).toInt();
+        hh = String(receivedChars).substring(10, 12).toInt();
+        mm = String(receivedChars).substring(12, 14).toInt();
+        ss = String(receivedChars).substring(14, 16).toInt();
+
+        setTime(hh, mm, ss, DD, MM, YYYY);
+        rtc.adjust(DateTime(YYYY, MM, DD, hh, mm, ss));
+
+      } else if (String(receivedChars)[1] == 'O') {
+        //sets the override thresholds in format:SOTH.HTL.LHH.HHL.L
+        TH = String(receivedChars).substring(2, 6).toFloat();
+        TL = String(receivedChars).substring(6, 10).toFloat();
+        HH = String(receivedChars).substring(10, 14).toFloat();
+        HL = String(receivedChars).substring(14, 18).toFloat();
+
+      } else if (String(receivedChars)[1] == 'R') {
+        //set the override relay states
+        //send SRSTART
+        //then the relay states in order
+        //then SRSTOP
+
+      } else if (String(receivedChars)[1] == 'P') {
+        //set main Program or ToDo list
+        //send SPSTART
+        //Then the relay state should be list, in chronological order
+        //Then SPSTOP
+      }
+
+
+
+    } else if (String(receivedChars).charAt(0) == 'M') {
+      //do manual control things
+      if (String(receivedChars).charAt(1) == '0') {
+        MC = 0;
+        Serial.println("MCOFF");
+      } else if (String(receivedChars).charAt(1) == '1') {
+        MC = 1;
+        Serial.println("MCON");
+      } else if (String(receivedChars).charAt(1) == 'R') {
+        RelayState = String(receivedChars).substring(2);
+        Serial.println(receivedChars);
+      }
+
+
     }
-        newData = false;
+
+
+  }
+  newData = false;
 }
 
 
 
 
 //this checks the ToDo list, and uses the override state to figure out what the relay syate should be, then sets RelayState
-void determineRS_shouldbe(){
+void determineRS_shouldbe() {
   //is MC enabled?
 
-    if(MC == 0){
-      //RS = prog + OR in the sense that a 2 in the OR leaves the program dictated relay state unchanged, but a 1 or 0 overrights it
-      for(int i=0;i<RelayState.length()-1;i++){
-        if(ORStates[ORState_num][i]=='2'){
-          //do not replace the corrosponding entry in relaystate
-          } else {
-            //need to select the appropriate OR list
-            RelayState.setCharAt(i,ORStates[ORState_num][i]);
-          }
+  if (MC == 0) {
+    //RS = prog + OR in the sense that a 2 in the OR leaves the program dictated relay state unchanged, but a 1 or 0 overrights it
+    for (int i = 0; i < RelayState.length() - 1; i++) {
+      if (ORStates[ORState_num][i] == '2') {
+        //do not replace the corrosponding entry in relaystate
+      } else {
+        //need to select the appropriate OR list
+        RelayState.setCharAt(i, ORStates[ORState_num][i]);
       }
     }
   }
+}
 
 
 
 
-void update_RS(){
-  if(RelayState != last_RelayState){
-    //shift out the relay state
-    }
-  
+void update_RS() {
+  if (RelayState != last_RelayState) {
+
+    char firstbyte[9];
+    RelayState.substring(0, 8).toCharArray(firstbyte, 9);
+    char lastbyte[9];
+    RelayState.substring(8, 16).toCharArray(lastbyte, 9);
+    RS_first = strtol(firstbyte, NULL, 2);
+    RS_last = strtol(lastbyte, NULL, 2);
+
+
+    digitalWrite(SO_clockPin, LOW);
+    digitalWrite(SO_latchPin, LOW);
+    shiftOut(SO_dataPin, SO_clockPin, LSBFIRST, RS_last); //relays 9-16
+    shiftOut(SO_dataPin, SO_clockPin, LSBFIRST, RS_first); //relays 1-8
+    digitalWrite(SO_latchPin, HIGH);
+
   }
 
+}
 
 
+void getSensorData() {
+  int timenow = now();
+  if (timenow > last_sensor_read + 2) {
+
+    int chk1 = DHT.read22(DHT22_PIN_1);
+    if (chk1 == DHTLIB_OK) {
+      t1 = (DHT.temperature * 1.8) + 32;
+      h1 = DHT.humidity;
+    } else {
+      t1 = 0.00;
+      h1 = 0.00;
+    }
 
 
+    int chk2 = DHT.read22(DHT22_PIN_2);
+    if (chk2 == DHTLIB_OK) {
+      t2 = (DHT.temperature * 1.8) + 32;
+      h2 = DHT.humidity;
+    } else {
+      t2 = 0.00;
+      h2 = 0.00;
+    }
+    last_sensor_read = now();
 
+  }
+}
     
